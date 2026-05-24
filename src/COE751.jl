a279e906150ca6caa7812a31a146baa3b514ff22
viniciusjusten <viniciusjusten@psr-inc.com>
2026-05-24 16:33:49 -0300

    refactor

--- src/COE751.jl
+++ src/COE751.jl
@@ -3,7 +3,10 @@ module COE751
 using EnumX
 using SparseArrays
 
-include.(readdir(joinpath(@__DIR__, "power_flow"), join = true))
+include("power_flow/collections.jl")
+include("power_flow/remote_voltage_control/reactive_injection.jl")
+include("power_flow/utils.jl")
+include("power_flow/solver.jl")
 include("utils.jl")
 include("newton_raphson.jl")
 
